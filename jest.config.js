14820a2b5a69fbc11a83408f8f3d3bd014844c9f
MatheusValera <lrfp1000@.gmail.com>
2022-07-06 10:50:12 -0300

    chore: exclude coverage in config files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+    '!<rootDir>/**/src/data/usercases/add-account/db-add-account-protocols.ts',
+    '!<rootDir>/**/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/**/src/presentation/protocols/index.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
