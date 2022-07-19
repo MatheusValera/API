6e70b92641baea883a0b7af68148eaf35cc2999e
MatheusValera <lrfp1000@.gmail.com>
2022-07-19 11:26:41 -0300

    feat: ensure LogMongoRepository creates an error log on sucess

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usercases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
@@ -12,5 +13,6 @@ export const makeSignupController = (): Controller => {
   const accountMongo = new AccountMongoRepository()
   const db = new DbAddAccount(encrypt, accountMongo)
   const signupController = new SignUpController(email, db)
-  return new LogControllerDecorator(signupController)
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signupController, logErrorRepository)
 }
